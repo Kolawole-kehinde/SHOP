8bfe5d9386656831b33bfcf0c4d97e716e33d2e8
kolawole-kehinde <kolawoledhikrullah93@gmail.com>
2025-03-04 00:30:50 +0100

    toggleMenu added to the NavBar

--- Shop/src/Components/layouts/Menu.jsx
+++ Shop/src/Components/layouts/Menu.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 
 const Menu = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <menu className='flex flex-col items-start space-y-6 p-4'>
+                 {
+            navRoutes.map(({name, path, id}) => (
+             <li key={id}>
+                <NavLink
+              
+                to={path}
+                className={({ isActive }) =>
+                  `inline-block px-4 font-semibold  duration-200 ${
+                    isActive ? "text-red-500" : "text-white"
+                  }`
+                }
+              >
+                {name}
+              </NavLink>
+            
+
+             </li>
+            ))
+          }
+               </menu>
+    </>
   )
 }
 
--- Shop/src/Components/layouts/Navbar/Navbar.jsx
+++ Shop/src/Components/layouts/Navbar/Navbar.jsx
@@ -66,27 +66,7 @@ const NavBar = () => {
                 <IoClose fontSize={30}/>
              </button>
                </div>
-               <menu className='flex flex-col items-start space-y-6 p-4'>
-                 {
-            navRoutes.map(({name, path, id}) => (
-             <li key={id}>
-                <NavLink
               
-                to={path}
-                className={({ isActive }) =>
-                  `inline-block px-4 font-semibold  duration-200 ${
-                    isActive ? "text-red-500" : "text-white"
-                  }`
-                }
-              >
-                {name}
-              </NavLink>
-            
-
-             </li>
-            ))
-          }
-               </menu>
             </nav>
           )
         }
